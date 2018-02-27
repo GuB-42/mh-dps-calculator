6f2a37541e13eac3978e65cedf68ded6802c1cf8
GuB-42 <guillaume.boujasson@gmail.com>
2018-02-27 16:11:42 +0100

    mhwg.org data for weapon, merge

---
+++ mhw_data/gen_weapons.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+./extract_kira.pl \
+kiranico_extracts/weapons/bow \
+kiranico_extracts/weapons/charge-blade \
+kiranico_extracts/weapons/dual-blades \
+kiranico_extracts/weapons/great-sword \
+kiranico_extracts/weapons/gunlance \
+kiranico_extracts/weapons/hammer \
+kiranico_extracts/weapons/heavy-bowgun \
+kiranico_extracts/weapons/hunting-horn \
+kiranico_extracts/weapons/insect-glaive \
+kiranico_extracts/weapons/lance \
+kiranico_extracts/weapons/light-bowgun \
+kiranico_extracts/weapons/long-sword \
+kiranico_extracts/weapons/switch-axe \
+kiranico_extracts/weapons/sword > /tmp/kira_weap.xml
+
+./extract_mhwg_weap.pl \
+mhwg_extracts/weapons/4013.html \
+mhwg_extracts/weapons/4009.html \
+mhwg_extracts/weapons/4003.html \
+mhwg_extracts/weapons/4000.html \
+mhwg_extracts/weapons/4007.html \
+mhwg_extracts/weapons/4004.html \
+mhwg_extracts/weapons/4012.html \
+mhwg_extracts/weapons/4005.html \
+mhwg_extracts/weapons/4010.html \
+mhwg_extracts/weapons/4006.html \
+mhwg_extracts/weapons/4011.html \
+mhwg_extracts/weapons/4001.html \
+mhwg_extracts/weapons/4008.html \
+mhwg_extracts/weapons/4002.html > /tmp/mhwg_weap.xml
+
+./merge_weapons.pl translations.xml /tmp/kira_weap.xml /tmp/mhwg_weap.xml
